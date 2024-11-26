28b10030109d2da5d2609bf417c32022cd183f7e
Michael Burdick <asylumrunner@gmail.com>
2024-11-26 16:31:04 -0600

    Dockerize API

--- api/server.py
+++ api/server.py
@@ -21,3 +21,6 @@ def get_conjugation():
     moods = payload['moods'] if 'moods' in payload else ['Indicative', 'Subjunctive']
     tenses = payload['tenses'] if 'tenses' in payload else ['Present', 'Future', 'Imperfect', 'Preterite', 'Conditional', 'Present Perfect', 'Future Perfect', 'Past Perfect', 'Preterite (Archaic)', 'Conditional Perfect']
     return verb_data_manager.get_conjugation(moods, tenses)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',debug=True)
